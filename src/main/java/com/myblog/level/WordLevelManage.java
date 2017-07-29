894b9c045359b9675ffb73cc1a03037a854be70f
Administrator <Administrator@PC-20170524NZZP>
2017-07-29 22:05:00 +0800

    词汇量

---
+++ src/main/java/com/myblog/level/WordLevelManage.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.myblog.level;
+
+/**
+ * @author Administrator
+ *
+ */
+public class WordLevelManage {
+
+	/**
+	 * 
+	 */
+	public WordLevelManage() {
+	}
+
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//1.读取一个stageWordsFile 到 wordlist(word,level)
+		//2.update到数据库
+	}
+
+}
