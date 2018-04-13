a3bd365d5db69be216a79c92e29d1ebcfaaf6fca
whatofit <whatofit@163.com>
2018-04-13 17:24:59 +0800

    把文章中的所有单词按照单词level分层

---
+++ src/main/java/com/myblog/statistics/WordsLayer.java
@@ -0,0 +1,18 @@
+package com.myblog.statistics;
+
+//把文章中的所有单词按照单词level分层
+//分层：一级上/下(小学),二级上/下(初中),三级上/下(高中),四级上/下(大学)，五级单词
+//二中输出模式(不输出低层级单词)：1.先按照层级排序，再按照词频排序；2先按照词频输出，再按照层级输出
+public class WordsLayer {
+
+    public WordsLayer() {
+    }
+
+    public static void main(String[] args) {
+        //1.把阶段stageFiles文件中单词读取到5个集合中出来
+        //2.把指定文件的单词统计出来
+
+        //3.把2中的单词映射到1中的stageFiles中，并输出。
+    }
+
+}
