b8ccb1df134fbe816aad4dccb3fc8b08d63c8d3d
whatofit <whatofit@163.com>
2018-04-28 17:47:25 +0800

    统计词频

---
+++ src/main/java/com/myblog/statistics/DictionaryTree2.java
@@ -0,0 +1,101 @@
+package com.myblog.statistics;
+
+//https://baike.baidu.com/item/字典树/9825209?fr=aladdin&fromid=517527&fromtitle=Trie树
+    
+//Trie树：统计词频、排序、查找
+//Trie树利用字符串的公共前缀降低了查询时间的开销，提高了查询的效率。
+//字典树的插入，删除和查找都非常简单，用一个一重循环即可。
+//1. 从根节点开始一次搜索
+//2. 取得要查找关键词的第一个字母，并根据该字母选择对应的子树并转到该子树继续进行检索
+//3. 在相应的子树上，取得要查找关键词的第二个字母，并进一步选择对应的子树进行检索
+//4. 迭代过程...
+//5. 在某个节点处，关键词的所有字母已被取出，则读取附在该节点上的信息，即完成查找
+public class DictionaryTree2 {
+    private int SIZE = 26;
+    private TreeNode root;// 字典的根
+
+    public DictionaryTree2() {
+        root = new TreeNode();
+    }
+
+    private class TreeNode {
+        private int num; // 词频统计
+        private TreeNode[] son;// 每一层都是由26字母开头的，即所有的节点
+        private boolean isWord;// 是不是最后一个节点
+        private char val;// 节点的值
+
+        TreeNode() {
+            num = 1;
+            son = new TreeNode[SIZE];
+            isWord = false;
+        }
+    }
+
+    public void insert(String str) {
+        if (str == null || str.length() == 0)
+            return;
+        TreeNode node = root;
+        char[] letters = str.toCharArray();
+        for (int i = 0, len = str.length(); i < len; i++) {
+            int pos = letters[i] - 'a';
+            if (node.son[pos] == null) {
+                node.son[pos] = new TreeNode();
+                node.son[pos].val = letters[i];
+            } else {
+                node.son[pos].num++;
+            }
+            node = node.son[pos];
+        }
+        node.isWord = true;
+    }
+
+    public int countNums(String str) { // 计算单词的数量
+        if (str == null || str.length() == 0) {
+            return -1;
+        }
+        TreeNode node = root;
+        char[] letters = str.toCharArray();
+        for (int i = 0, len = str.length(); i < len; i++) {
+            int pos = letters[i] - 'a';
+            if (node.son[pos] == null) {
+                return 0;
+            } else {
+                node = node.son[pos];
+            }
+        }
+        return node.num;
+    }
+
+    // 在字典树中查找一个完全匹配的单词.
+    public boolean search(String str) {
+        if (str == null || str.length() == 0) {
+            return false;
+        }
+        TreeNode node = root;
+        char[] letters = str.toCharArray();
+        for (int i = 0, len = str.length(); i < len; i++) {
+            int pos = letters[i] - 'a';
+            if (node.son[pos] != null) {
+                node = node.son[pos];
+            } else {
+                return false;
+            }
+        }
+        return node.isWord;
+    }
+
+    public TreeNode getRoot() {
+        return this.root;
+    }
+
+    public static void main(String[] args) {
+        DictionaryTree2 tree = new DictionaryTree2();
+        String[] strs = { "beer", "banana", "band", "bee", "you", "young", "that" };
+        for (String str : strs) {
+            tree.insert(str);
+        }
+        System.out.println(tree.search("bee"));
+        System.out.println(tree.countNums("ba"));
+
+    }
+}
--- src/main/java/com/myblog/statistics/WordStatistics.java
+++ src/main/java/com/myblog/statistics/WordStatistics.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -46,8 +47,9 @@ public class WordStatistics {
 //        output(infoIds);
 //        // output2(map);
 
-        //getAllWord();
-        getDistinctWord();
+//        getAllWord();
+        getAllWord2();
+//        getDistinctWord();
     }
 
     // java统计一段英文中单词及个数/统计各个单词出现的次数
@@ -59,7 +61,8 @@ public class WordStatistics {
         while (mr.find()) {
             String strWord = mr.group();
             // 转为小写
-            String key = strWord.toLowerCase();
+            //String key = strWord.toLowerCase();
+            String key = strWord;
             if (map.containsKey(key)) {
                 map.put(key, map.get(key) + 1);
             } else {
@@ -109,7 +112,7 @@ public class WordStatistics {
     // 找出全文的单词，转小写，并排序
     public static void getAllWord() {
         try {
-            String fileName = "WordStatistics.txt";// 文件路径
+            String fileName = "/WordStatistics.txt";// 文件路径
             String resFileName = Constant.PATH_RESOURCES + fileName;
             BufferedReader br = new BufferedReader(new FileReader(resFileName));
             List<String> words = br.lines().flatMap(line -> Stream.of(line.split(" ")))
@@ -122,7 +125,56 @@ public class WordStatistics {
         }
 
     }
+    
+    public static void getAllWord2() {
+        String fileName = "/WordStatistics.txt";// 文件路径
+        String resFileName = Constant.PATH_RESOURCES + fileName;
+        String fileBody = ResourceUtil.readFile(resFileName);
+        String[] wordArr = fileBody.split("[^a-zA-Z]");  //过滤出只含有字母的
+        List<String> wordList = new ArrayList<String>();  //存储过滤后单词的列表    
+        for (String word : wordArr) {    
+            if(word.length() != 0){  //去除长度为0的行    
+                wordList.add(word);    
+            }    
+        }
+        Map<String, Integer> wordsCount = StatisticsFreq(wordList);
+        SortMap(wordsCount);
+        System.out.println(wordsCount.size());
+    }
 
+    //统计词频
+    public static Map<String, Integer> StatisticsFreq(List<String> wordList){    
+        Map<String, Integer> wordsCount = new TreeMap<String,Integer>();  //存储单词计数信息，key值为单词，value为单词数         
+        
+        //单词的词频统计    
+        for (String li : wordList) {    
+            if(wordsCount.get(li) != null){    
+                wordsCount.put(li,wordsCount.get(li) + 1);    
+            }else{    
+                wordsCount.put(li,1);    
+            }    
+    
+        }    
+        return wordsCount;
+    }    
+    
+    //按value的大小进行排序    
+    public static void SortMap(Map<String,Integer> oldmap){    
+            
+        ArrayList<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String,Integer>>(oldmap.entrySet());    
+            
+        Collections.sort(list,new Comparator<Map.Entry<String,Integer>>(){    
+            @Override    
+            public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {    
+                return o2.getValue() - o1.getValue();  //降序    
+            }    
+        });    
+            
+        for(int i = 0; i<list.size(); i++){    
+            System.out.println(list.get(i).getKey()+ ": " +list.get(i).getValue());    
+        }       
+    }    
+    
     // 找出全文的单词，转小写，并排序,使用 distinct 来找出不重复的单词。
     public static List<String> getDistinctWord() {
         try {
