cac58a3f7a623c802906b2cf93d40e17e437c97f
Administrator <Administrator@PC-20170524NZZP>
2018-05-31 02:47:53 +0800

    文件路径读配置

---
+++ src/main/java/com/myblog/rtf2txt/ParseDoc2Txt.java
@@ -0,0 +1,16 @@
+package com.myblog.rtf2txt;
+
+import com.myblog.Constant;
+
+public class ParseDoc2Txt {
+
+	public ParseDoc2Txt() {
+	}
+
+	public static void main(String[] args) {
+		//E:\FanMingyou\workspace\workspace-sts-3.9.2.RELEASE_2017-12-27\LevelWordWithFreq\src\main\resources\the old exam\英语一\1980-2015年考研英语(一)历年真题及答案解析.doc
+		String filePath = Constant.PATH_RESOURCES + "/the old exam/英语一/1980-2015年考研英语(一)历年真题及答案解析.doc";
+		RtfUtil.doc2Text(filePath);
+	}
+
+}
--- src/main/java/com/myblog/statistics/MergeDuplicateCOCA.java
+++ src/main/java/com/myblog/statistics/MergeDuplicateCOCA.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import com.myblog.Constant;
+import com.myblog.util.CfgUtil;
 import com.myblog.util.RegEx;
 import com.myblog.util.ResourceUtil;
 
@@ -16,33 +17,38 @@ public class MergeDuplicateCOCA {
 
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
-		String cocaFile = Constant.PATH_RESOURCES + "/美国当代英语语料库COCA词频20000_words.txt";
+		// 1.读取单词集合路径
+		String cfg_englist_path = CfgUtil.getPropCfg(Constant.FILE_CONFIG_FILE, "cfg_englist_txt");
+		// String forderPath = "E:/FanMingyou/The Economist";
+		String cocaFile = Constant.PATH_RESOURCES + cfg_englist_path;
 		// 1.读取加载coca文件
 		List<String> cocaFileLines = ResourceUtil.readFileLines(cocaFile);
 		// 2.处理coca原文件集合为word list
 		List<String> wordLines = new ArrayList<String>();
-//		for (String curFileLine : cocaFileLines) {// 待循环(可能重复)列表
-//			String[] fileLine = curFileLine.split("\t");
-//			boolean isAdd = true;// is Add Or Update
-//			for (int i = 0; i < wordLines.size(); i++) {
-//				String curWordLine = wordLines.get(i);
-//				String[] wordLine = curWordLine.split("\t");
-//				if (fileLine[1].equalsIgnoreCase(wordLine[1])) {
-//					isAdd = false;
-//					String newWordLine = wordLine[0] + "/" + fileLine[0] + "\t" + wordLine[1] + "\t" + wordLine[2] + "/"
-//							+ fileLine[2];
-//					wordLines.set(i, newWordLine);
-//					break;
-//				}
-//			}
-//			if (isAdd) {
-//				wordLines.add(curFileLine);// 添加到list末尾
-//			}
-//		}
-		Map<String, String> mapWords =wordList2SetWithMerge(cocaFileLines);
+		// for (String curFileLine : cocaFileLines) {// 待循环(可能重复)列表
+		// String[] fileLine = curFileLine.split("\t");
+		// boolean isAdd = true;// is Add Or Update
+		// for (int i = 0; i < wordLines.size(); i++) {
+		// String curWordLine = wordLines.get(i);
+		// String[] wordLine = curWordLine.split("\t");
+		// if (fileLine[1].equalsIgnoreCase(wordLine[1])) {
+		// isAdd = false;
+		// String newWordLine = wordLine[0] + "/" + fileLine[0] + "\t" + wordLine[1] +
+		// "\t" + wordLine[2] + "/"
+		// + fileLine[2];
+		// wordLines.set(i, newWordLine);
+		// break;
+		// }
+		// }
+		// if (isAdd) {
+		// wordLines.add(curFileLine);// 添加到list末尾
+		// }
+		// }
+		Map<String, String> mapWords = wordList2SetWithMerge(cocaFileLines);
 		wordLines = SortMap(mapWords);
 		// 3.保存word list
-		String cocaWordFile = Constant.PATH_RESOURCES + "/美国当代英语语料库COCA词频20000_words_unique.txt";
+		String cfg_englist_txt_result = CfgUtil.getPropCfg(Constant.FILE_CONFIG_FILE, "cfg_englist_txt_result");
+		String cocaWordFile = Constant.PATH_RESOURCES + cfg_englist_txt_result;
 		ResourceUtil.writerFile(cocaWordFile, wordLines, false);
 		long endTime = System.currentTimeMillis();
 		System.out.println("执行耗时 : " + (endTime - startTime) / 1000f + " 秒 ");
@@ -52,25 +58,27 @@ public class MergeDuplicateCOCA {
 	public static Map<String, String> wordList2SetWithMerge(List<String> cocaFileLines) {
 		Map<String, String> mapWords = new HashMap<String, String>();
 		for (String curFileLine : cocaFileLines) {// 待循环(可能重复)列表
-			String[] fileLine = curFileLine.split("\t");
+			String[] fileLine = curFileLine.split("\\s");// \t
 			String strWord = fileLine[1];
 			// 转为小写
 			String key = strWord.toLowerCase();
-			//String key = strWord;
+			// String key = strWord;
 			if (mapWords.containsKey(key)) {
 				String oldWord = mapWords.get(key);
-				String[] wordLine = oldWord.split("\t");
-				String newWordLine = wordLine[0] + "/" + fileLine[0] + "\t" + wordLine[1] + "\t" + wordLine[2] + "/"
-				+ fileLine[2];
+				String[] wordLine = oldWord.split("\\s");// \t
+				String newWordLine = wordLine[0] + "/" + fileLine[0] + "\t" + wordLine[1];
+				if (wordLine.length > 2) {
+					newWordLine += "\t" + wordLine[2] + "/" + fileLine[2];
+				}
 				mapWords.put(key, newWordLine);
 			} else {
 				mapWords.put(key, curFileLine);
 			}
 		}
-		
+
 		return mapWords;
 	}
-	
+
 	// 按频率的大小进行排序
 	public static List<String> SortMap(Map<String, String> oldmap) {
 
@@ -81,15 +89,15 @@ public class MergeDuplicateCOCA {
 			public int compare(Entry<String, String> obj1, Entry<String, String> obj2) {
 				int o1 = RegEx.catchFirstNumber(obj1.getValue());
 				int o2 = RegEx.catchFirstNumber(obj2.getValue());
-				return o1  - o2; // 降序
+				return o1 - o2; // 降序
 			}
 		});
 
-		List<String > wordFreqList = new ArrayList<String>();
-		
+		List<String> wordFreqList = new ArrayList<String>();
+
 		for (int i = 0; i < list.size(); i++) {
 			wordFreqList.add(list.get(i).getValue());
-			//System.out.println(list.get(i).getKey() + ": " + list.get(i).getValue());
+			// System.out.println(list.get(i).getKey() + ": " + list.get(i).getValue());
 		}
 		return wordFreqList;
 	}
